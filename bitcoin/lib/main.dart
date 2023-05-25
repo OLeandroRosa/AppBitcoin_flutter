124e6d2814051ba074a97cff12e4c54293c9628b
adm <admin>
2023-05-25 20:44:36 -0300

    App BitCoin Flutter (Alunos: Leandro Rosa, Luan Gomes, Francianny Leite)

---
+++ bitcoin/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:bitcoin/views/bitcoin.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Bitcoin(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
